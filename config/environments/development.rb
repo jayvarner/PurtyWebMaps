4b055e6c55d818077cfa935bd38b7206cca04f38
Jay Varner <jayvarner@gmail.com>
2016-02-09 23:37:02 -0500

    Got ActionCable responding.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  Rails.application.config.action_cable.allowed_request_origins = ['http://192.168.200.128:3000']
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
